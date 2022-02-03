82d0315bd7f584c0cc4c519de2286d9c46762ba3
Samson-Dai <dscsamson@gmail.com>
2022-02-02 22:01:28 -0500

    Update local path
    improve the potability

--- run_linear_reg.sh
+++ run_linear_reg.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-#SBATCH -p RM-shared
-#SBATCH -t 48:00:00
-#SBATCH --ntasks-per-node=2
+##SBATCH -p RM-shared
+##SBATCH -t 00:10:00
+##SBATCH --ntasks-per-node=2
 
 export NXF_OPTS="-Xms500M -Xmx2G" # limit NF to 2 GB of memory
 
-data_dir="/Users/Administrator/Desktop/samson1/CMU/year1_summer/research/nextflow/distributed_linear_regresion/example_data.rds"
+# sets up the offsite directory structure
+source ~/.research_config
+
+data_dir=$LOCAL_LINEAR_REG_DATA_DIR"example_data.rds"
 k=10
-result_dir="/Users/Administrator/Desktop/samson1/CMU/year1_summer/research/nextflow/distributed_linear_regresion/result"
-work_dir="/Users/Administrator/Desktop/samson1/CMU/year1_summer/research/nextflow/distributed_linear_regresion/work"
+result_dir=$LOCAL_LINEAR_REG_DATA_DIR"results"
+work_dir=$LOCAL_LINEAR_REG_DATA_DIR"work"
 #######################
 # Run Nextflow pipeline
 #######################
 rm -f trace.txt
 
 # nextflow run $glmeiv_nf_pipeline \
-nextflow run nextflow -r main \
+nextflow run https://github.com/Samson-Dai/distributed_linear_regresion -r main \
 --data $data_dir \
 --k $k \
 --result_dir $result_dir \
